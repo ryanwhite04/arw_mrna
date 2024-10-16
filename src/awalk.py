625102b94ea8be3b49ab126bc27d7653fc610f37
Ryan White <ryanwhite04@gmail.com>
2024-10-16 22:02:37 +0800

    Replace adaptive random walk with generator
    verbose is false for use on server
    Can pass a seed for the random choices for testing
    best_fitness instead of prev_fitness

--- src/awalk.py
+++ src/awalk.py
@@ -1,98 +1,61 @@
 """ Implements adaptive random walks on CDSs. """
-from typing import Callable, List, Optional
+from typing import Callable, List, Optional, Generator
 from .protein import CodonFrequencyTable, random_cds
 import random
-import dataclasses
+from dataclasses import dataclass, asdict
 
-
-@dataclasses.dataclass
+@dataclass
 class WalkConfig:
     aa_seq: str
     freq_table: CodonFrequencyTable
     objective: Callable[[List[str]], float]
     steps: int
-    init_cds: Optional[List[str]] = None
+    init_cds: Optional[List[str]] = None,
     verbose: bool = False
+    seed: Optional[int] = None
 
+@dataclass
+class Measures:
+    CAI: float
+    AUP: Optional[float] = None
+    EFE: Optional[float] = None
 
-@dataclasses.dataclass
+@dataclass
 class WalkResult:
     cds: List[str]
+    step: int
+    measures: Measures
     fitness: float
+    best_fitness: float
+    
+    def to_dict(self):
+        return asdict(self)
 
-
-def adaptive_random_walk(config: WalkConfig):
+def adaptive_random_walk(config: WalkConfig) -> Generator[WalkResult, None, None]:
     """Runs ARW to maximize objective function on CDSs."""
-    cds = random_cds(
-        config.aa_seq, config.freq_table) if config.init_cds is None else config.init_cds
-    prev_fitness = config.objective(cds)
+    if config.seed is not None:
+        random.seed(config.seed)
+    cds = config.init_cds
+    if cds is None:
+        cds = random_cds(config.aa_seq, config.freq_table, config.seed)
+    # cds = random_cds(config.aa_seq, config.freq_table, config.seed) if config.init_cds is None else config.init_cds,
+    best_fitness, measures = config.objective(cds)
     if config.verbose:
         print(f"Initial CDS: {cds}")
+    yield WalkResult(cds, 0, measures, best_fitness, best_fitness)
     for step in range(config.steps):
         mutidx = random.randint(0, len(cds) - 1)
         while len(config.freq_table.get_codons(config.aa_seq[mutidx])) == 1:
             mutidx = random.randint(0, len(cds) - 1)
-        mutcodons = list(config.freq_table.get_codons(
-            config.aa_seq[mutidx])-set([cds[mutidx]]))
+        mutcodons = list(config.freq_table.get_codons(config.aa_seq[mutidx]) - set([cds[mutidx]]))
         mutcodon = random.choice(mutcodons)
         new_cds = cds[:mutidx] + [mutcodon] + cds[mutidx+1:]
-        new_fitness = config.objective(new_cds)
-        if new_fitness > prev_fitness:
+        fitness, measures = config.objective(new_cds)
+        if fitness > best_fitness:
             cds = new_cds
-            prev_fitness = new_fitness
+            best_fitness = fitness
             if config.verbose:
                 print(f"New CDS: {new_cds}")
         if config.verbose:
-            print(
-                f"Step: {step}, Fitness: {new_fitness}, Best Fitness: {prev_fitness}")
-    return WalkResult(cds, prev_fitness)
-
-
-def adaptive_random_walk_generator(config: WalkConfig):
-    """Runs ARW to maximize objective function on CDSs."""
-    cds = random_cds(
-        config.aa_seq, config.freq_table) if config.init_cds is None else config.init_cds
-    prev_fitness, measures = config.objective(cds)
-    if config.verbose:
-        yield {
-            "type":"initial",
-            "cds": cds,
-            "fitness": prev_fitness,
-            "measures": measures
-        }
-    for step in range(config.steps):
-        mutidx = random.randint(0, len(cds) - 1)
-        while len(config.freq_table.get_codons(config.aa_seq[mutidx])) == 1:
-            mutidx = random.randint(0, len(cds) - 1)
-        mutcodons = list(config.freq_table.get_codons(
-            config.aa_seq[mutidx]) - set([cds[mutidx]]))
-        mutcodon = random.choice(mutcodons)
-        new_cds = cds[:mutidx] + [mutcodon] + cds[mutidx+1:]
-        new_fitness, measures = config.objective(new_cds)
-        if new_fitness > prev_fitness:
-            cds = new_cds
-            prev_fitness = new_fitness
-            if config.verbose:
-                yield {
-                    "type": "new_cds",
-                    "cds": new_cds,
-                    "fitness": new_fitness,
-                    "step": step,
-                    "best_fitness": prev_fitness,
-                    "measures": measures
-                }
-        if config.verbose:
-            yield {
-                "type": "progress",
-                "step": step,
-                "fitness": new_fitness,
-                "best_fitness": prev_fitness,
-                "measures": measures
-            }
-    yield {
-        "type": "final",
-        "cds": cds,
-        "step": config.steps - 1,
-        "fitness": prev_fitness,
-        "measures": measures
-    }
+            print(f"Step: {step}, Fitness: {fitness}, Best Fitness: {best_fitness}")
+        yield WalkResult(cds, step+1, measures, fitness, best_fitness)
